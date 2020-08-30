6ea57d2419080c09c37e65dd34b79aadf06d2128
soumen-gif <soumentaki1996@gmail.com>
2020-08-30 16:59:45 +0530

    Added: Ability to check that happy or sad

--- src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
+++ src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
@@ -3,7 +3,10 @@ package com.bridgelabz.MoodAnalyser;
 public class MoodAnalyser {
 
     public String analyseMood(String message) {
+        if (message.contains("SAD"))
         return "SAD";
+        else
+            return "HAPPY";
 
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -12,4 +12,10 @@ public class MoodAnalyserTest {
         String mood = md.analyseMood("This is a sad message");
         Assert.assertEquals("SAD",mood);
     }
+    @Test
+    public void testHappyMood()  {
+        MoodAnalyser md = new MoodAnalyser();
+        String mood = md.analyseMood("This is a happy message");
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
